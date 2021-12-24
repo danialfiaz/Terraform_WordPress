cf1e967086df6984ddd0b12ce4cabc4218bc5eb2
danial2310 <danialfiaz96@gmail.com>
2021-12-24 16:59:18 +0500

    code

---
+++ modules/RDS/variable.tf
@@ -0,0 +1,63 @@
+variable "identifier" {
+
+}
+
+
+variable "allocated_storage" {
+
+}
+
+
+variable "storage_type" {
+
+}
+
+
+variable "engine" {
+
+}
+
+
+variable "engine_version" {
+
+}
+
+
+variable "instance_class" {
+
+}
+
+
+variable "name" {
+
+}
+
+
+variable "username" {
+
+}
+
+
+variable "password" {
+
+}
+
+
+
+variable "public" {
+
+}
+
+
+variable "vpc_id" {
+
+}
+
+variable "parameter_group_name" {
+    
+}
+
+
+variable "wordpress_sg" {
+   
+}
